1cd804115477c827e3499a7ce6833f9ae0270742
Milos Mitrovic <milosmitrovic005@gmail.com>
2024-09-29 23:00:04 +0200

    fixing errors

--- submit_order.php
+++ submit_order.php
@@ -5,6 +5,12 @@ include 'db_connect.php'; // Use the existing connection from db_connect.php
 // Get the POST data from the request
 $data = json_decode(file_get_contents("php://input"), true);
 
+// Check if required fields are present
+if (!isset($data['first_name'], $data['last_name'], $data['address'], $data['city'], $data['zip_code'], $data['phone'], $data['cartItems'])) {
+    echo json_encode(['success' => false, 'error' => 'Nedostaju potrebni podaci']);
+    exit;
+}
+
 try {
     // Begin transaction
     $conn->begin_transaction();
@@ -29,12 +35,12 @@ try {
     // Bind values
     $stmt->bind_param(
         "ssssssii",
-        $data['ime'],
-        $data['prezime'],
-        $data['adresa'],
-        $data['grad'],
-        $data['postanski_broj'],
-        $data['broj_telefona'],
+        $data['first_name'],
+        $data['last_name'],
+        $data['address'],
+        $data['city'],
+        $data['zip_code'],
+        $data['phone'],
         $ukupnaCena,
         $cenaDostave
     );
