83adb70972e0ffca24ee329a4fe96506067f5eec
Milos Mitrovic <milosmitrovic005@gmail.com>
2024-09-30 23:41:10 +0200

    fixing errors

--- submit_order.php
+++ submit_order.php
@@ -1,4 +1,9 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Include your database connection
 include 'db_connect.php'; // Use the existing connection from db_connect.php
 
