d9ec17a417277a8fc979c8989508014d87daf28d
Milos Mitrovic <milosmitrovic005@gmail.com>
2024-10-08 00:33:07 +0200

    fixing

--- shop-full-width.php
+++ shop-full-width.php
@@ -40,6 +40,7 @@ if ($queryString) {
 
 // Display the filtered products count
 $productCount = count($filteredProducts);
+print_r($filteredProducts);
 ?>
 
 <!DOCTYPE html>
@@ -207,7 +208,7 @@ $productCount = count($filteredProducts);
                     </div>
                     <div class="flex flex-wrap col-50 mb-[-24px]">
                         <?php if ($productCount > 0): ?>
-                            <?php foreach ($filteredProducts as $product): print_r($product)?>
+                            <?php foreach ($filteredProducts as $product): ?>
                                 <div class="min-[992px]:w-[25%] w-[50%] max-[480px]:w-full px-[12px] cr-product-box mb-[24px]">
                                     <div class="cr-product-card h-full p-[12px] border-[1px] border-solid border-[#e9e9e9] bg-[#fff] rounded-[5px] overflow-hidden flex-col max-[480px]:w-full">
                                         <div class="cr-product-image rounded-[5px] flex items-center justify-center relative">
