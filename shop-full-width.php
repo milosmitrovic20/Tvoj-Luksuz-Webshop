31c3cf742e6d95880106ca83c8002df659c91873
Milos Mitrovic <milosmitrovic005@gmail.com>
2024-10-08 08:30:42 +0200

    changing mapping

--- shop-full-width.php
+++ shop-full-width.php
@@ -34,7 +34,7 @@ $filteredProducts = $products; // Default to all products
 function normalizeSerbianLatin($text) {
     // Mapping for common Latin to Serbian Latin characters
     $charMap = [
-        'č' => 'c', 'š' => 's', 'ž' => 'z', 'đ' => 'dj', 'đ' => 'd', 'ć' => 'c'
+        'c' => 'č', 's' => 'š', 'z' => 'ž', 'dj' => 'đ', 'd' => 'đ', 'c' => 'ć'
     ];
 
     // Replace the Latin characters with Serbian equivalents
